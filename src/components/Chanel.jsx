703e6041a4086c0ca142460f7d2c63d284a34f56
ibrohimain <ibrohimabdullayev576@gmail.com>
2024-09-04 12:28:38 +0500

    dfdsarfgt

--- src/components/Chanel.jsx
+++ src/components/Chanel.jsx
@@ -7,14 +7,14 @@ const Chanel = () => {
     <div className="w-full chanel-bg pt-20">
       <div className="w-full h-[650px] chanel flex items-center justify-center">
         <div className="w-[85%] h-[60%] flex flex-wrap">
-          <div className="w-full lg:w-1/2 h-full lg:h-full flex flex-col items-center justify-center">
+          <div className="w-full lg:w-1/2 h-[50%] lg:h-full flex flex-col items-center justify-center">
             <p className="text-white text-center">«Baqara» surasi, 196-oyat</p>
             <div className="w-[100px] h-0 border-[2px] border-[#00806b] mt-6 mb-4"></div>
             <h2 className="text-white text-[20px] sm:text-[30px] md:text-[34px] lg:text-[38px] xl:text-[40px] font-semibold text-center">
               «Alloh uchun Haj va Umrani tugal ado eting»
             </h2>
           </div>
-          <div className="w-full lg:w-1/2 h-full flex items-center justify-center">
+          <div className="w-full lg:w-1/2 h-[50%] flex items-center justify-center">
             <div className="w-[100%] sm:w-[70%] md:w-[60%] h-[60%]">
               <ul className="w-full grid grid-cols-3 gap-4">
                 <a href="https://t.me/AL_Saray_Travel" target="_blank" rel="noopener noreferrer">
